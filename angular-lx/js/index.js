d7e65ed014f17d6c12bb9254392b0aa4a99cb0ad
Administrator <Administrator@zhaoyongfeng>
2014-08-04 14:32:33 +0800

    修改过后

--- angular-lx/js/index.js
+++ angular-lx/js/index.js
@@ -1,2 +1,25 @@
 // 为核心的 list 服务创建模块
-var showList = angular.module( "list", [] )
+var showList = angular.module( "list", ["ngRoute"] );
+
+// 在 URL ,模板和控制器之间建立映射关系
+function umRouteConfig ( $routeProvider ) {
+	$routeProvider
+	.when( "/", {
+		controller:Listcontent,
+		templateUrl:"list.html"
+	})
+	.otherwise({
+		redirectTo:"/"
+	})
+}
+
+showList.config( umRouteConfig );
+
+
+// 请求数据
+function Listcontent ( $scope, $http ) {
+	$http.get( "user.json" ).success( function ( data ) {
+		console.log(data);
+		$scope.lists = data;
+	})
+}
